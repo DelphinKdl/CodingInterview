02838096e27802d11e04aa5892aff66d1fd52a78
abdulazizasiri <aasiri@lion.lmu.edu>
2018-07-21 13:37:39 -0700

    Update ReverseString.java

---
+++ Leetcode/Easy/Strings/ReverseString.java
@@ -0,0 +1,13 @@
+public class ReverseString {
+har[] s = orig.toCharArray();
+    int n = s.length;
+    int halfLength = n / 2;
+    for (int i=0; i<halfLength; i++)
+    {
+        char temp = s[i];
+        s[i] = s[n-1-i];
+        s[n-1-i] = temp;
+    }
+    return new String(s);
+    }
+}
