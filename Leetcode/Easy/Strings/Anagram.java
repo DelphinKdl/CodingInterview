e6880ddcf82861a128626c85004e67107d916085
abdulazizasiri <aasiri@lion.lmu.edu>
2018-07-22 19:16:27 -0700

    added some sol, but they do not help solve the problem

---
+++ Leetcode/Easy/Strings/Anagram.java
@@ -0,0 +1,12 @@
+import java.util.Hashtable;
+public class Anagram {
+  public boolean isAnagram(String s, String t) {
+      char [] sElements = s.toCharArray(); 
+       return true;
+   }
+public static void main(String [] args){
+  Anagram a = new Anagram();
+  boolean test = a.isAnagram("ab","a");
+  System.out.println(test);
+}
+}
