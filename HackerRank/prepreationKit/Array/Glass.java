65221c27ebf48498a7b4fd922346699de43eceb3
abdulazizasiri <aasiri@lion.lmu.edu>
2018-08-28 17:56:56 -0700

    2D array problem hackerrank

--- HackerRank/prepreationKit/Array/Glass.java
+++ HackerRank/prepreationKit/Array/Glass.java
@@ -1,13 +1,32 @@
 public class Glass{
+  static void Checkit(int [][] temp){
+
+  }
   public static void main (String [] args){
-    int [][]arr = {
-      {1 ,1 ,1 ,0 ,0 ,0},
+    int [][]a = {
+      {1 ,1 ,1 ,0 ,0 ,0},// We Want to run it 4 timess.
       {0, 1 ,0 ,0 ,0 ,0},
       {1 ,1, 1 ,0 ,0 ,0},
       {0 ,0, 2 ,4 ,4 ,0},
       {0 ,0, 0 ,2 ,0 ,0},
       {0 ,0 ,1 ,2 ,4 ,0},
-  };
-}
+    };
 
+    int max = Integer.MIN_VALUE;
+
+    int length = a.length-2;
+           for(int i=0; i<length; i++){
+               for(int j=0; j<length; j++){
+                   int val=a[i][j]+ a[i][j+1] + a[i][j+2] + a[i+1][j+1] + a[i+2][j]+ a[i+2][j+1] + a[i+2][j+2];
+                   System.out.println(val);
+                   if (val==5){
+                     System.out.println("Khmsa");
+                   }
+                   if(val>max){
+                       max=val;
+                   }
+               }
+           }
+           System.out.println(max);
+}
 }
