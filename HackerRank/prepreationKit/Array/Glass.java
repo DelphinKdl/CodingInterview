78b0ced10527dd0678597c50f054e8389316cbb0
abdulazizasiri <aasiri@lion.lmu.edu>
2018-08-28 18:02:27 -0700

    added a new set of numbers

--- HackerRank/prepreationKit/Array/Glass.java
+++ HackerRank/prepreationKit/Array/Glass.java
@@ -1,15 +1,13 @@
 public class Glass{
-  static void Checkit(int [][] temp){
-
-  }
   public static void main (String [] args){
-    int [][]a = {
-      {1 ,1 ,1 ,0 ,0 ,0},// We Want to run it 4 timess.
-      {0, 1 ,0 ,0 ,0 ,0},
-      {1 ,1, 1 ,0 ,0 ,0},
-      {0 ,0, 2 ,4 ,4 ,0},
-      {0 ,0, 0 ,2 ,0 ,0},
-      {0 ,0 ,1 ,2 ,4 ,0},
+
+      int [][]a = {
+      {-9 ,-9 ,-9  ,1 ,1 ,1 },// We Want to run it 4 timess.
+      { 0 ,-9  ,0  ,4 ,3 ,2},
+      {-9 ,-9, -9 , 1 ,2 ,3},
+      { 0  ,0  ,8  ,6 ,6 ,0},
+      { 0  ,0 , 0 ,-2 ,0 ,0},
+      { 0  ,0  ,1  ,2 ,4 ,0},
     };
 
     int max = Integer.MIN_VALUE;
@@ -19,9 +17,6 @@ public class Glass{
                for(int j=0; j<length; j++){
                    int val=a[i][j]+ a[i][j+1] + a[i][j+2] + a[i+1][j+1] + a[i+2][j]+ a[i+2][j+1] + a[i+2][j+2];
                    System.out.println(val);
-                   if (val==5){
-                     System.out.println("Khmsa");
-                   }
                    if(val>max){
                        max=val;
                    }
