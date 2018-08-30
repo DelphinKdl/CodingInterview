f4a20158ef0cb42d7b156b112df55e0d70b78d8a
abdulazizasiri <aasiri@lion.lmu.edu>
2018-08-30 01:49:07 -0700

    not done with image rotate

---
+++ Leetcode/Easy/Arrays/ImageRotate.java
@@ -0,0 +1,14 @@
+public class ImageRotate {
+  public static void swapingDiagonally(int [] [] matrix){
+
+
+  }
+  public static void main (String [] args){
+    int [][] matrix =  {{1,2,3},
+                        {4,5,6},
+                        {7,8,9}
+    };
+    ImageRotate.swapingDiagonally(matrix);
+
+}
+}
